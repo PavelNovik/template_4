b07b39c01dfe0c62fd4186562d5e0183dfd918cb
PavelNovik <brn2befree@gmail.com>
2022-10-27 16:48:38 +0300

    now js

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,11 +1,19 @@
 'use strict';
-
-console.log('Hello world');
+// console.log('Hello world');
+const users = [];
 
 const headerLogo = document.querySelector('.header__logo');
 const footerLogo = document.querySelector('.footer__logo');
 const headerNavItems = document.querySelectorAll('.header__navigation_item');
 const requestForm = document.querySelector('.request__form');
+const formInputs = requestForm.elements;
+
+// console.log(requestForm);
+// console.log(formInputs);
+
+// for (let el of formInputs) {
+//   console.log(el.value);
+// }
 
 // console.log(headerNavItems, headerLogo);
 
@@ -15,4 +23,39 @@ headerLogo.addEventListener('click', function (e) {
   console.log(e.target);
 });
 
-function sendDataForm() {}
+requestForm.addEventListener('submit', getDataForm);
+
+function getDataForm(e) {
+  e.preventDefault();
+  const user = {};
+  const userName = requestForm.fullName.value;
+  const userEmail = requestForm.email.value;
+  const userPhone = requestForm.phone.value;
+  const userCountry = requestForm.country.value;
+  const userLegacy = requestForm.rules.checked;
+  if (userLegacy) {
+    user.name = userName;
+    user.mail = userEmail;
+    user.phone = userPhone;
+    user.country = userCountry;
+    users.push(user);
+    console.log(user, users);
+  }
+
+  console.log(
+    userLegacy
+      ? `Добрый день, уважаемый ${userName}. На вашу почту: ${userEmail} было отправленно сообщение. Так же на ваш номер телефона ${userPhone} в течении суток должно прийти СМС с кодом доступа к вашему аккаунту. Ваша страна: ${userCountry} учавствует в нашей программе.`
+      : `К сожалению вы не можете продолжить без согласия.`
+  );
+
+  for (let el of formInputs) {
+    if (el.type === 'submit') continue;
+    if (el.type === 'checkbox') {
+      el.checked = false;
+      continue;
+    }
+    el.value = '';
+  }
+}
+// const reqForm = document.forms.userForm;
+// console.log(reqForm.elements);
