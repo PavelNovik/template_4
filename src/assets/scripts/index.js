baf8c284dc681b46ff07de09f7b50c4690027c04
PavelNovik <brn2befree@gmail.com>
2022-10-27 11:22:53 +0300

    Template design is finished

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,18 @@
+'use strict';
+
 console.log('Hello world');
+
+const headerLogo = document.querySelector('.header__logo');
+const footerLogo = document.querySelector('.footer__logo');
+const headerNavItems = document.querySelectorAll('.header__navigation_item');
+const requestForm = document.querySelector('.request__form');
+
+// console.log(headerNavItems, headerLogo);
+
+headerLogo.addEventListener('click', function (e) {
+  console.log(`Now you press ${e}, the target of this elements is ${e.target}`);
+  console.log(e);
+  console.log(e.target);
+});
+
+function sendDataForm() {}
